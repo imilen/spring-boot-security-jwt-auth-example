81be2cac50d1e30598e67ecc4c71f3d5b498fef5
imilen <m.ivanov@email.com>
2026-06-23 17:12:27 +0300

    first commit

---
+++ src/main/java/com/spring_boot_security_jwt_auth_example/SpringBootSecurityJwtAuthExampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_security_jwt_auth_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityJwtAuthExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityJwtAuthExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_boot_security_jwt_auth_example/SpringBootSecurityJwtAuthExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_security_jwt_auth_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityJwtAuthExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
